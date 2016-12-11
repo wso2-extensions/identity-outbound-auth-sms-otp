70e30a790aa9795c319d4ca9f66b392fe1ff70cf
Darshana Gunawardana <darshanasbg@users.noreply.github.com>
2016-12-11 12:54:52 +0530

    ISCONNECT-43: Changed the display name of SMSOTPAuthenticator
    $subject to align with display names of other authenticators while providing better UX

--- component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/SMSOTPConstants.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/SMSOTPConstants.java
@@ -21,7 +21,7 @@ package org.wso2.carbon.identity.authenticator.smsotp;
 
 public class SMSOTPConstants {
     public static final String AUTHENTICATOR_NAME = "SMSOTP";
-    public static final String AUTHENTICATOR_FRIENDLY_NAME = "SMSOTP";
+    public static final String AUTHENTICATOR_FRIENDLY_NAME = "SMS OTP";
     public static final String ALGORITHM_NAME = "SHA1PRNG";
     public static final String ALGORITHM_HMAC = "HmacSHA1";
     public static final String ALGORITHM_HMAC_SHA = "HMAC-SHA-1";
