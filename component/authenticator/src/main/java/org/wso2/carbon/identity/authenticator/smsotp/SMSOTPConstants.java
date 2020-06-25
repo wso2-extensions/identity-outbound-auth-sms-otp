ea556f51048950b05e124fa89b464c56814c84e8
AnuradhaSK <anuradha199528@gmail.com>
2020-06-25 12:21:33 +0530

    Support account locking for SMS OTP

--- component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/SMSOTPConstants.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/SMSOTPConstants.java
@@ -36,6 +36,11 @@ public class SMSOTPConstants {
     public static final String SAVED_OTP_LIST = "http://wso2.org/claims/otpbackupcodes";
     public static final String USER_SMSOTP_DISABLED_CLAIM_URI = "http://wso2.org/claims/identity/smsotp_disabled";
     public static final String BACKUP_CODES_SEPARATOR = ",";
+    public static final String SMS_OTP_FAILED_ATTEMPTS_CLAIM = "http://wso2.org/claims/identity/failedSmsOtpAttempts";
+    public static final String FAILED_LOGIN_LOCKOUT_COUNT_CLAIM =
+            "http://wso2.org/claims/identity/failedLoginLockoutCount";
+    public static final String ACCOUNT_LOCKED_CLAIM = "http://wso2.org/claims/identity/accountLocked";
+    public static final String ACCOUNT_UNLOCK_TIME_CLAIM = "http://wso2.org/claims/identity/unlockTime";
 
     public static final String SMS_URL = "sms_url";
     public static final String HTTP_METHOD = "http_method";
@@ -53,6 +58,9 @@ public class SMSOTPConstants {
     public static final String IS_ENABLE_ALPHANUMERIC_TOKEN = "EnableAlphanumericToken";
     public static final String TOKEN_EXPIRY_TIME = "TokenExpiryTime";
     public static final String TOKEN_LENGTH = "TokenLength";
+    public static final String SHOW_AUTH_FAILURE_REASON = "ShowAuthFailureReason";
+    public static final String ENABLE_ACCOUNT_LOCKING_FOR_FAILED_ATTEMPTS = "EnableAccountLockingForFailedAttempts";
+
 
     public static final String GET_METHOD = "GET";
     public static final String POST_METHOD = "POST";
@@ -94,10 +102,17 @@ public class SMSOTPConstants {
     public static final String BACKWARD = "backward";
     public static final String SCREEN_VALUE = "&screenvalue=";
     public static final String CODE_MISMATCH = "codeMismatch";
-    public static final String ORDER_OF_DIGITS = "&order=";
+    public static final String ACCOUNT_LOCKED = "accountLocked";
+    public static final String ACCOUNT_LOCKED_ERROR = "account.locked";
     public static final String TOKEN_VALIDITY_TIME = "tokenValidityTime";
     public static final String SENT_OTP_TOKEN_TIME = "sentOTPTokenTime";
     public static final String TOKEN_EXPIRED = "tokenExpired";
     public static final String TOKEN_EXPIRED_VALUE = "token.expired";
+    public static final String LOCAL_AUTHENTICATOR = "LOCAL";
 
+    public static final String PROPERTY_LOGIN_FAIL_TIMEOUT_RATIO = "account.lock.handler.login.fail.timeout.ratio";
+    public static final String PROPERTY_ACCOUNT_LOCK_ON_FAILURE = "account.lock.handler.enable";
+    public static final String PROPERTY_ACCOUNT_LOCK_ON_FAILURE_MAX = "account.lock.handler.On.Failure.Max.Attempts";
+    public static final String PROPERTY_ACCOUNT_LOCK_TIME = "account.lock.handler.Time";
+    public static final String ADMIN_INITIATED = "AdminInitiated";
 }
--- component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/internal/SMSOTPAuthenticatorServiceComponent.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/smsotp/internal/SMSOTPAuthenticatorServiceComponent.java
@@ -24,12 +24,20 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.identity.application.authentication.framework.ApplicationAuthenticator;
 import org.wso2.carbon.identity.authenticator.smsotp.SMSOTPAuthenticator;
+import org.wso2.carbon.identity.governance.IdentityGovernanceService;
+import org.wso2.carbon.identity.handler.event.account.lock.service.AccountLockService;
 import org.wso2.carbon.user.core.service.RealmService;
 
 import java.util.Hashtable;
 
 /**
  * @scr.component name="identity.application.authenticator.SMSOTP.component" immediate="true"
+ * @scr.reference name="IdentityGovernanceService"
+ * interface="org.wso2.carbon.identity.governance.IdentityGovernanceService" cardinality="1..1"
+ * policy="dynamic" bind="setIdentityGovernanceService" unbind="unsetIdentityGovernanceService"
+ * @scr.reference name="AccountLockService"
+ * interface="org.wso2.carbon.identity.handler.event.account.lock.service.AccountLockService"
+ * cardinality="1..1" policy="dynamic" bind="setAccountLockService" unbind="unsetAccountLockService
  */
 public class SMSOTPAuthenticatorServiceComponent {
 
@@ -72,4 +80,23 @@ public class SMSOTPAuthenticatorServiceComponent {
         SMSOTPAuthenticatorServiceComponent.realmService = null;
     }
 
+    protected void setIdentityGovernanceService(IdentityGovernanceService idpManager) {
+
+        SMSOTPServiceDataHolder.getInstance().setIdentityGovernanceService(idpManager);
+    }
+
+    protected void unsetIdentityGovernanceService(IdentityGovernanceService idpManager) {
+
+        SMSOTPServiceDataHolder.getInstance().setIdentityGovernanceService(null);
+    }
+
+    protected void setAccountLockService(AccountLockService accountLockService) {
+
+        SMSOTPServiceDataHolder.getInstance().setAccountLockService(accountLockService);
+    }
+
+    protected void unsetAccountLockService(AccountLockService accountLockService) {
+
+        SMSOTPServiceDataHolder.getInstance().setAccountLockService(null);
+    }
 }
